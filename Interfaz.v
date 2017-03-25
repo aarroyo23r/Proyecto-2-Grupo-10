edc019341c5689f7c4297f29e6409ede64544c1a
aarroyo23r <aarroyo23r@gmail.com>
2017-03-25 02:00:50 -0600

    Modificacion de escritura

--- Interfaz.v
+++ Interfaz.v
@@ -21,12 +21,10 @@
 
 
 module Interfaz( //Definicion entradas y salidas
-    input wire clk,
+    input wire clk,pixel_x,pixel_y,
     input wire temporizador, // Indica si el temporizador esta activo
     input wire temporizadorFin,//Indica cuando finaliza el temporizador
     input [7:0] datoRTC,//Dato proveniente del RTC
-    input pixel_x,
-    input pixel_y,
     input [2:0] cursor//Indica la posicion en la que se encuentra el cursor
     );
 
@@ -41,35 +39,21 @@ wire tick=0;//Tick para guardar datos mientras se refresca la pantalla, para que
 localparam tamContador;//Tamaño del contador de datos guardados
 reg [3:0] contGuardados=0;//Cuenta los datos guardados
 reg [10:0] dirAsciiDato;//Contiene la direccion Ascii del dato proveniente del RTC
-wire w;//Habilita el modo escritura de los registros
-wire r;//Habilita el modo lectura de los registros
+wire w,r;//Habilitan el modo escritura o lectura de los registros respectivamente
 
 //Registros con las direcciones de memoria
 //Direcciones de los datos de RTC
 //Reloj
-reg [6:0] centesimas;
-reg [6:0] Segundos;
-reg [6:0] minutos;
-reg [6:0] horas;
-reg [6:0] fecha;
-reg [6:0] mes;
-reg [6:0] año;
-reg [6:0] diaSemana;
-reg [6:0] numeroSemana;
-//Temporizador
-reg [6:0] centesimasT;
-reg [6:0] segundosT;
-reg [6:0] minutosT;
-reg [6:0] horasT;
-//Direcciones Datos extra
+reg [6:0] centesimas,Segundos,minutos,horas,fecha,mes,año,diaSemana,numeroSemana;
 
+//Temporizador
+reg [6:0] centesimasT,SegundosT,minutosT,horasT;
+//Direcciones Datos extra
 
 
 //Selector de Direcciones
 reg [10:0] dirMemoria;//Almacena la direccion de memoria
 reg [3:0] filaMemoria;//Cambio entre las filas de la memoria
-
-
 
 
 //Mux recorrido columnas Memoria
