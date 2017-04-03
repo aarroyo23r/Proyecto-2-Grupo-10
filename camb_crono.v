d6a6392c7601cc3505491db72c4a8d5b6d344ec3
jennifercm <jencamora@hotmail.com>
2017-04-03 12:46:14 -0600

    Add files via upload

--- camb_crono.v
+++ camb_crono.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 26.03.2017 23:39:38
+// Create Date: 26.03.2017 23:28:17
 // Design Name: 
-// Module Name: Reiniciar_crono
+// Module Name: camb_crono
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,11 +20,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Reiniciar_crono(
+module camb_crono(
     input wire clk,
     input wire enable,
+    input wire [7:0] dir_in,
     output wire [7:0] dir_out,
-    output wire reinicio,
+    output wire camb_crono,
     output wire WR,
     output wire RD,
     output wire ready,
@@ -32,22 +33,23 @@ module Reiniciar_crono(
     );
     
     reg [8:0] clkr=9'b000000000;
-    reg [2:0] next, state; 
+    reg [2:0] next, state;
     reg  [7:0] dir_out_reg=0;
-    reg RD_reg=0, WR_reg=0, ready_reg=0, reinicio_reg=0; 
+    reg RD_reg=0, WR_reg=0, ready_reg=0, camb_crono_reg=0;
+        
     always @(posedge enable)begin
-        dir_out_reg=8'b00000000;
         RD_reg<=0;
         WR_reg<=0;
         ready_reg=0;
+        next<=3'b000;
         state=0;
         clkr=0;
-        reinicio_reg=1;
-        next<=3'b001;end 
+        camb_crono_reg=1;
+        end 
     
     always @(posedge clk)
-        if(!enable)begin
-        reinicio_reg=0;
+    if(!enable)begin
+        camb_crono_reg=0;
         ready_reg=0;
         RD_reg=0;
         WR_reg=0;
@@ -56,27 +58,26 @@ module Reiniciar_crono(
     begin
         case (state)
             3'b000: begin 
-                   dir_out_reg=dir_out_reg+1;
+                    if (dir_in==4 || dir_in==1 || dir_in==2 || dir_in==3) dir_out_reg=dir_in;
+                    else dir_out_reg=1;
                    next=3'b001; end
             3'b001: begin
-                   next=3'b010;end
+                   next=3'b010;
+                   RD_reg=1; end
             3'b010: begin
                    if (RD_reg==0)begin
                      next=3'b011;end 
                    end 
             3'b011: begin
                    if (WR_reg==0)begin
+                      ready_reg=1;
                       next=3'b100;
                       end 
                    end 
-            3'b100: begin if (dir_out_reg==8'b00000100)begin
-                                    ready_reg=1;
-                                    dir_out_reg=0;
-                                    next=2'b00;
-                                    end
-                                  else
-                                    next=2'b00; 
-                                  end 
+            3'b100: begin
+                     ready_reg=1;
+                     next=3'b000;
+                   end
         endcase
     end
     
@@ -104,10 +105,11 @@ module Reiniciar_crono(
         clkr=0;end
     end end
     end
-assign RD=RD_reg;
-assign WR=WR_reg;
-assign ready=ready_reg;
-assign dir_out=dir_out_reg;
-assign reinicio=reinicio_reg;    
-    
+ 
+ assign RD=RD_reg;
+ assign WR=WR_reg;
+ assign ready=ready_reg;
+ assign dir_out=dir_out_reg;
+ assign camb_crono=camb_crono_reg;
+ 
 endmodule
