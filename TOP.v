c083f6b7feac6d1197099e7d4616efd29269a4c8
Emmanuel Araya Gamboa <emmanuelaraya114@gmail.com>
2017-04-21 01:14:18 -0600

    Update TOP.v

--- TOP.v
+++ TOP.v
@@ -77,19 +77,25 @@ wire [7:0]datos9;
 wire [7:0]datos10;
 
 //reg [7:0]segundos,minutos,horas,date,num_semana,mes,ano,dia_sem;
+wire [7:0]contador2;
 
 MaquinaEscritura Escritura_unit(.clk(clk),.RW(RW),.Crono(Crono),.Inicio(Inicio1),.Reset(Reset),.push_arriba(push_arriba),
                                 .push_abajo(push_abajo),.push_izquierda(push_izquierda),.push_derecha(push_derecha),.address(ADDRESS_write),
                                 .data_mod(data_mod),.reset2(reset3),.segundos(segundos),.minutos(minutos),.horas(horas),.date(date),
-                                .num_semana(num_semana),.mes(mes),.ano(ano),.dia_sem(dia_sem),.Per_read(Per_read),.Escribir(Escribir)); //falta ingresar datos
+                                .num_semana(num_semana),.mes(mes),.ano(ano),.dia_sem(dia_sem),.Per_read(Per_read),.Escribir(Escribir),.contador_todo(contador2)); //falta ingresar datos
 wire [7:0]address;
 reg [7:0] address2;
+wire [7:0] data_inicio;
+wire [7:0] address_inicio;
+
+inicializacion inicio(.clk(clk),.Inicio1(Inicio1),.address(address_inicio),.data_mod(data_inicio));
 
 //assign address=(Escribir | reset3)? ADDRESS_write:ADDRESS_read;
+reg [7:0] data_mod2;
 
 always@(posedge clk)
 begin
-    if(Escribir|(reset3 |!RW))
+    if((Escribir|(reset3 |!RW)) && (!Inicio1))
         begin
         address2<=ADDRESS_write;
         if(Reset)
@@ -105,9 +111,7 @@ begin
 
 
 wire [7:0] data_vga; //datos hacia controlador_vga
-wire [7:0]contador2;
 wire [7:0] data_intermedia;
-reg [7:0] data_mod2;
 
 always@(posedge clk)
     begin
@@ -115,18 +119,19 @@ always@(posedge clk)
        begin
        data_mod2<=data_reset;
        end
-       else
+    if((!RW |Escribir |reset3)& !Inicio1)
+        begin
        data_mod2<=data_mod;
-    if(!RW |Escribir |Inicio1 |reset3)
-       data_mod2<=data_mod;
-    begin
-    
-    end
+        end
+    if(Inicio1)
+        begin
+        data_mod2<=data_inicio;
+        end    
     end
 
 Protocolo_rtc Proto_unit(.clk(clk),.address(address2),.DATA_WRITE(data_mod2),.IndicadorMaquina(RW),
                          .ChipSelect(ChipSelect),.Read(Read),.Write(Write),.AoD(AoD),.DATA_ADDRESS(DATA_ADDRESS),
-                         .data_vga(data_intermedia),.contador_todo(contador2));
+                         .data_vga(data_intermedia),.contador_todo(contador2),.Inicio1(Inicio1));
 
 //REGISTROS PARA ENTREGAR DATA A CONTROLADOR VGA
 reg [7:0] data_vga_entrada;
@@ -151,7 +156,10 @@ Registros Register_unit(.clk(clk),.bit_inicio1(bit_inicio1),.data_vga(data_inter
 
 
 
-//Interfaz Interfaz_unit(.clk(clk),.reset(Reset),.rgbO(rgbO),.resetSync(Reset),.inicioSecuencia(bit_inicio1),.datoRTC(data_out),.hsync(hsync),.vsync(vsync),.video_on(video_on),
-                       // .datos0(segundos),.datos1(minutos),.datos2(horas),.datos3(date),.datos4(mes),.datos5(ano),.datos6(dia_sem),.datos7(num_semana),
-                        //.datos8(datos8),.datos9(datos9),.datos10(datos10),.instrucciones(instrucciones)
+Interfaz Interfaz_unit(.clk(clk),.reset(Reset),.rgbO(rgbO),.resetSync(Reset),.inicioSecuencia(bit_inicio1),.datoRTC(data_out),.hsync(hsync),.vsync(vsync),.video_on(video_on),
+                        .datos0(segundos),.datos1(minutos),.datos2(horas),.datos3(date),.datos4(mes),.datos5(ano),.datos6(dia_sem),.datos7(num_semana),
+                        .datos8(datos8),.datos9(datos9),.datos10(datos10),.instrucciones(instrucciones)
 
+                        );
+
+endmodule
